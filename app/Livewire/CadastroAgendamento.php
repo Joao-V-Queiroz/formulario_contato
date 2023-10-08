645594c5efb3077e23805ddc55d1060f070e3735
João Victor Queiroz Silva <joaovsq@hotmail.com>
2023-10-07 21:13:53 -0300

    ajustes e pequenas alterações, encerrando projeto

--- app/Livewire/CadastroAgendamento.php
+++ app/Livewire/CadastroAgendamento.php
@@ -3,9 +3,31 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use WireUi\Traits\Actions;
 
 class CadastroAgendamento extends Component
 {
+    use Actions;
+
+    public array $rules = [
+
+    ];
+
+    protected array $messages = [
+
+    ];
+
+    public function submit():void
+    {
+      $this->validade();
+
+      //enviar um e-mail
+
+      //guardar no banco de dados
+
+      $this->notification()->success('Cadastro realizado com sucesso!','Agenda marcada');
+      $this->reset('');
+    }
 
     public function render()
     {
